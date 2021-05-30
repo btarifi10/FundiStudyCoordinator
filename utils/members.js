de04041ffc3bd02a6e01b97aa47e263708c315b1
Nathan Jones <natmax.jones@gmail.com>
2021-05-30 12:11:35 +0200

    feat(group chat): Enable non-persistent group chat
    - Implemented using socket.io
    - Users can select a group to join and chat with other uses in the same group
    - Chat history is NOT saved between sessions
    - Basic styling achieved using Bootstrap and a custom chat.css file
    
    close #20

---
+++ utils/members.js
@@ -0,0 +1,42 @@
+// TODO - Replace with database implementation to make chat persistent
+const members = []
+
+// NOTE:
+// Currently this array stores active members across ALL the group chats
+// and members are retrieved using their id and filtering functions
+const membersInChat = []
+
+// Adds a member to the group chat and returns them as a member object
+function addChatMember (id, username, group) {
+  const member = { id, username, group }
+  membersInChat.push(member)
+  return member
+}
+
+// Gets the member in the chat based on their id
+function getCurrentMember (id) {
+  return membersInChat.find(member => member.id === id)
+}
+
+// Removes a member from the chat based on their id and returns them
+function removeChatMember (id) {
+  const index = membersInChat.findIndex(member => member.id === id)
+
+  // If a member with that id was found...
+  if (index !== -1) {
+    // ...remove and return them
+    return membersInChat.splice(index, 1)[0]
+  }
+}
+
+// Get all the members in the chat
+function getChatMembers (group) {
+  return membersInChat.filter(member => member.group === group)
+}
+
+module.exports = {
+  addChatMember,
+  getCurrentMember,
+  removeChatMember,
+  getChatMembers
+}
