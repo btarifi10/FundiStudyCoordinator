e118f37725f5a04fe90759e5b0047356da5d90cb
Basheq Tarifi <btarifi08@gmail.com>
2021-05-30 23:53:50 +0200

    refactor(login): use environment variable for PORT
    Install dotenv for the use of environment variables while in dev mode

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 'use strict'
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const express = require('express')
 const path = require('path')
@@ -10,6 +13,6 @@ const loginRouter = require('./loginRouter.js')
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/', loginRouter)
 
-const port = 6969
+const port = process.env.PORT
 app.listen(port)
 console.log('Express server running on port', port)
