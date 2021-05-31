607ef9de290145df872a3f43ba5c3a1e6a67b63f
TaliWeinstein <tali.jae@gmail.com>
2021-05-31 12:35:16 +0200

    refactor(group selection): Commented out buggy Bootstrap code for option-selection

--- index.js
+++ index.js
@@ -4,30 +4,11 @@
 - This allows the user to have multiple dropdowns without any conflict */
 
 const dropdown = document.getElementsByClassName('dropdown-btn')
+const dropMenu = document.getElementsByClassName('dropdown-container')
 const studyGroups = ['Big Data', 'Software 3', '19th Centuary Spanish Poetry']
 
-// for (let i = 0; i < studyGroups.length; i++) {
-// const anchor = document.createElement('a')
-// anchor.href = '#'
-//  anchor.appendChild(document.createTextNode(studyGroups[i])
-//  document.body.appendChild(anchor)
-// }
-// const dropMenu = document.getElementsByClassName('dropdown-container')
-
-// dropdown.addEventListener('click', function () {
-//   for (let i = 0; i < studyGroups.length; i++) {
-//     const opt = studyGroups[i]
-//     const el = document.createElement('a')
-//     el.href = '#'
-//     // el.textContent = opt
-//     // el.value = opt
-//     el.appendChild(document.createTextNode(opt))
-//     document.body.appendChild(el)
-//   }
-// })
-
+// working select code
 const select = document.getElementById('studyGroup')
-
 for (let i = 0; i < studyGroups.length; i++) {
   const opt = studyGroups[i]
   const el = document.createElement('option')
@@ -35,6 +16,23 @@ for (let i = 0; i < studyGroups.length; i++) {
   el.value = opt
   select.appendChild(el)
 }
+
+// Buggy code for using the Bootstrap dropdown menu
+// Continuously adds Big Data to the list on every click
+
+// function populateGroups () {
+//   for (let i = 0; i < studyGroups.length; i++) {
+//     const groupName = studyGroups[i]
+//     console.log(groupName)
+//     const addedElement = document.createElement('a')
+//     addedElement.textContent = groupName
+//     addedElement.value = groupName
+//     addedElement.href = '#'
+//     if (dropMenu.length === studyGroups.length) { return } else {
+//       dropMenu[i].appendChild(addedElement)
+//     }
+//   }
+// }
 
 // for (let i = 0; i < dropdown.length; i++) {
 //   dropdown[i].addEventListener('click', function () {
