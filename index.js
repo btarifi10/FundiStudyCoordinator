a8744c2a7e9080355361a87e2c3ce944c28b0e79
TaliWeinstein <tali.jae@gmail.com>
2021-05-31 09:46:37 +0200

    feat(group select): Basic dropdown menu of study groups

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+'use strict'
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+/* Loop through  dropdown buttons to toggle between hiding and showing its dropdown content
+- This allows the user to have multiple dropdowns without any conflict */
 
-const port = process.env.PORT || 3000
+const dropdown = document.getElementsByClassName('dropdown-btn')
+const studyGroups = ['Big Data', 'Software 3', '19th Centuary Spanish Poetry']
 
-app.listen(port)
+// for (let i = 0; i < studyGroups.length; i++) {
+// const anchor = document.createElement('a')
+// anchor.href = '#'
+//  anchor.appendChild(document.createTextNode(studyGroups[i])
+//  document.body.appendChild(anchor)
+// }
+// const dropMenu = document.getElementsByClassName('dropdown-container')
 
-console.log('Express server running on port', port)
+// dropdown.addEventListener('click', function () {
+//   for (let i = 0; i < studyGroups.length; i++) {
+//     const opt = studyGroups[i]
+//     const el = document.createElement('a')
+//     el.href = '#'
+//     // el.textContent = opt
+//     // el.value = opt
+//     el.appendChild(document.createTextNode(opt))
+//     document.body.appendChild(el)
+//   }
+// })
+
+const select = document.getElementById('studyGroup')
+
+for (let i = 0; i < studyGroups.length; i++) {
+  const opt = studyGroups[i]
+  const el = document.createElement('option')
+  el.textContent = opt
+  el.value = opt
+  select.appendChild(el)
+}
+
+// for (let i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener('click', function () {
+//     this.classList.toggle('active')
+//     const dropdownContent = this.nextElementSibling
+
+//     if (dropdownContent.style.display === 'block') {
+//       dropdownContent.style.display = 'none'
+//     } else {
+//       dropdownContent.style.display = 'block'
+//     }
+//   })
+// }
