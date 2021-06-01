d5de4d659557f59d322ff69ec45458811ed81919
Basheq Tarifi <btarifi08@gmail.com>
2021-06-01 14:19:57 +0200

    test(login): test duplicate user behaviour
    Add following tests:
    - A second unique user can be registered
    - An existing user cannot be registered
    - The second user can also log in

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -60,6 +60,59 @@ describe('Login test with new registration and successive login and log out', ()
   })
 })
 
+describe('Multiple registered users', () => {
+  it('A second unique user can be registered', () => {
+    cy.visit('register')
+    cy.get('form')
+
+    cy.get('input[name="username"]')
+      .type('hazey')
+      .should('have.value', 'hazey')
+
+    cy.get('input[name="password"]')
+      .type('yaze')
+      .should('have.value', 'yaze')
+
+    cy.get('form').submit()
+
+    cy.url().should('eq', Cypress.config().baseUrl + 'login')
+  })
+
+  it('An existing user cannot be registered', () => {
+    cy.visit('register')
+    cy.get('form')
+
+    cy.get('input[name="username"]')
+      .type('hazey')
+      .should('have.value', 'hazey')
+
+    cy.get('input[name="password"]')
+      .type('yaze')
+      .should('have.value', 'yaze')
+
+    cy.get('form').submit()
+
+    cy.url().should('eq', Cypress.config().baseUrl + 'register')
+  })
+
+  it('The second user can also log in', () => {
+    cy.visit('login')
+    cy.get('form')
+
+    cy.get('input[name="username"]')
+      .type('hazey')
+      .should('have.value', 'hazey')
+
+    cy.get('input[name="password"]')
+      .type('yaze')
+      .should('have.value', 'yaze')
+
+    cy.get('form').submit()
+
+    cy.url().should('eq', Cypress.config().baseUrl + 'dashboard')
+  })
+})
+
 describe('Access to pages based on authentication', () => {
   it('Unauthenticated user cannot access dashboard', () => {
     cy.visit('dashboard')
