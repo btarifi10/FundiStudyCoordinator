7f8259303c22850a5b9c00e0b5cc1648cf3a645b
Nathan Jones <natmax.jones@gmail.com>
2021-06-03 23:35:37 +0200

    SQUASH

--- cypress/integration/memberships.spec.js
+++ cypress/integration/memberships.spec.js
@@ -1,23 +1,8 @@
+'use strict'
 import data from '../fixtures/memberships.json'
 const GROUPS = data.GROUPS
 const GROUPS2 = data.GROUPS2
-'use strict'
 function createStringComparisons (data) {
-  let firstTable = ''
-
-  data.forEach(function ({ group_id, group_name, date_joined, group_num, group_online, group_url }) {
-    firstTable += `${group_id}`
-    firstTable += `${group_name}` //  tableHtml += `<td>${group_name}</td>`;
-    firstTable += `${new Date(date_joined).toLocaleString()}`
-    firstTable += `${group_num}`
-    firstTable += `${group_online}`
-    firstTable += 'Navigate'
-    firstTable += 'Leave'
-  })
-  return firstTable
-}
-
-function createJSONStringComparisons (data) {
   let firstTable = ''
 
   data.forEach(function ({ group_id, group_name, date_joined, group_num, group_online, group_url }) {
@@ -50,7 +35,7 @@ describe('Profile testing links', () => {
   it('displays the membership information when the page loads', () => {
     // By using multiple different methods, it is ensured that nothing is left out from the code
     // Make sure that the term is found somewhere within the table
-    const fTable = createJSONStringComparisons(GROUPS)
+    const fTable = createStringComparisons(GROUPS)
     cy.get('#table-body')
       .invoke('text')
       .should((text1) => {
