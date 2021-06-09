a7c1d2ed5df0c08dbc12a5eb2fba8907c9c7d5d9
TaliWeinstein <tali.jae@gmail.com>
2021-06-09 16:26:04 +0200

    feat(search): Search all groups from database

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,16 @@
-'use strict'
 
+'use strict'
+import { UserService } from './UserService.js'
 const dropdown = document.getElementsByClassName('dropdown-btn')
+const dropdownAllGroups = document.getElementsByClassName('dropdown-btn2')
 const dropMenu = document.getElementById('dropdown-containerID')
+const dropMenuAllGroups = document.getElementById('dropdown-containerAllGroups')
 
 // To be replaced with database functionality therefore a more complex container was not implemented
 const studyGroups = ['Big Data', 'Software 3', 'Sociology']
 const hrefValues = ['big-data', 'software', 'sociology']
-
-// working naive select code
-// const select = document.getElementById('studyGroup')
-// for (let i = 0; i < studyGroups.length; i++) {
-//   const groupName = studyGroups[i]
-//   const addedElement = document.createElement('option')
-//   addedElement.textContent = groupName
-//   addedElement.value = groupName
-//   select.appendChild(addedElement)
-// }
+const userService = UserService.getUserServiceInstance()
+let currentUser = null
 
 function populateGroups () {
   for (let i = 0; i < studyGroups.length; i++) {
@@ -30,7 +25,7 @@ function populateGroups () {
   }
 }
 
-// Populates the inital fropdown menu selection with the study groups listed above and then subsequently highlights them
+// Populates the inital dropdown menu selection with the study groups listed above and then subsequently highlights them
 // when mouse is hovered over each item
 for (let i = 0; i < dropdown.length; i++) {
   populateGroups()
@@ -52,6 +47,71 @@ function groupSearch () {
   // Allows both cases to be correctly identified
   const filter = searchTerm.value.toLowerCase()
   const a = dropMenu.getElementsByTagName('a')
+  for (let i = 0; i < a.length; i++) {
+    const groupName = a[i].textContent || a[i].innerText
+    if (groupName.toLowerCase().indexOf(filter) > -1) {
+      a[i].style.display = ''
+    } else {
+      a[i].style.display = 'none'
+    }
+  }
+}
+
+// 'http://localhost:3000/profileViews'
+
+document.addEventListener('DOMContentLoaded', function () {
+  userService.getCurrentUser().then(
+    user => {
+      currentUser = user
+      const welcomeDiv = document.getElementById('welcome-div')
+      const welcomeHeading = document.createElement('h2')
+      welcomeHeading.textContent = `Welcome, ${currentUser.username} with ID ${currentUser.id}`
+      welcomeDiv.appendChild(welcomeHeading)
+    })
+  fetch('http://localhost:3000/get-groups')//  , {
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      populateAllGroups(data)
+    })
+})
+// fetch('http://localhost:3000/group-getter')//  , {
+//   .then(response => response.json())
+//   .then(data => {
+//     populateAllGroups(data)
+//   })
+
+function populateAllGroups (data) {
+  data.recordset.forEach(function ({ group_name }) {
+    const groupName = `${group_name}`
+    const addedElement = document.createElement('a')
+    addedElement.textContent = groupName
+    addedElement.value = groupName
+    // addedElement.href = hrefValues[i]
+    // addedElement.id = hrefValues[i]
+    addedElement.class = 'dropdown-item'
+    dropMenuAllGroups.appendChild(addedElement)
+  })
+}
+
+for (let i = 0; i < dropdownAllGroups.length; i++) {
+  dropdownAllGroups[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    const dropdownContent = this.nextElementSibling
+
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none'
+    } else {
+      dropdownContent.style.display = 'block'
+    }
+  })
+}
+
+function entireGroupSearch () {
+  const searchTerm = document.getElementById('inputGroupSearch')
+  // Allows both cases to be correctly identified
+  const filter = searchTerm.value.toLowerCase()
+  const a = dropMenuAllGroups.getElementsByTagName('a')
   for (let i = 0; i < a.length; i++) {
     const groupName = a[i].textContent || a[i].innerText
     if (groupName.toLowerCase().indexOf(filter) > -1) {
--- server/app.js
+++ server/app.js
@@ -44,6 +44,29 @@ app.use('/', loginRouter)
 const groupRouter = require('./group-routes')
 app.use(groupRouter)
 
+const db = require('./database-service')
+
+app.get('/get-groups', function (req, res) {
+  // Make a query to the database
+  db.pools
+    // Run query
+    .then((pool) => {
+      return pool.request()
+        // This is only a test query, change it to whatever you need
+        .query('SELECT group_name FROM groups')
+    })
+    // Send back the result
+    .then(result => {
+      res.send(result)
+    })
+    // If there's an error, return that with some description
+    .catch(err => {
+      res.send({
+        Error: err
+      })
+    })
+})
+
 /* ----------------------------- Yasser's Code ----------------------------- */
 
 app.get('/intermediate-group', function (req, res) {
@@ -145,29 +168,6 @@ io.on('connection', socket => {
 })
 
 /* ----------------------------- Database Test ----------------------------- */
-/*
-const db = require('./database-service')
 
-app.get('/database', function (req, res) {
-  // Make a query to the database
-  db.pools
-    // Run query
-    .then((pool) => {
-      return pool.request()
-        // This is only a test query, change it to whatever you need
-        .query('SELECT * FROM users')
-    })
-    // Send back the result
-    .then(result => {
-      res.send(result)
-    })
-    // If there's an error, return that with some description
-    .catch(err => {
-      res.send({
-        Error: err
-      })
-    })
-})
-*/
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT||3000
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
