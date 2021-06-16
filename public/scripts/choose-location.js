5712cbd799b853ccc88496cf6bc48fd6fb12be5b
tarryn-maggs <tarrynmaggs@gmail.com>
2021-06-16 20:35:29 +0200

    feat: Created a meetings page in which users can select to either create an online or face-to-face meeting

--- public/scripts/choose-location.js
+++ public/scripts/choose-location.js
@@ -18,51 +18,46 @@ const ADDR_REGEX = /[^A-z0-9À-ž'.,\s+-º]+/g
 
 /* ------------------------------ DOM Elements ------------------------------ */
 
-const addressInput = document.getElementById('addressInput')
-const mapFrame = document.getElementById('map')
+// const addressInput = document.getElementById('addressInput')
+// const mapFrame = document.getElementById('map')
 const meetingForm = document.getElementById('meeting-form')
 const addressList = document.getElementById('address-list')
+const meetingChoice = document.getElementById('selection')
+// const
 
 /* ------------------------------ Functionality ------------------------------ */
 
-// Dynamically update the map based on the text input
-addressInput.addEventListener('input', (event) => {
-  mapFrame.src = generateMapURL(event.target.value)
+// Update which meeting options should be displayed for the user
+meetingChoice.addEventListener('change', (event) => {
+  if (event.target.value == 'online') {
+    loadPlatform()
+  }
+  if (event.target.value == 'face-to-face') {
+    loadLocation()
+  }
+  if (event.target.value == 'none-selected') {
+    window.alert('Please select a viable option')
+  }
 })
 
-/*
-Convert submitted address to Google Map link
+// Dynamically update the map based on the text input
+document.querySelector('#place').addEventListener('input', function (event) {
+  if (event.target.id == 'addressInput') {
+    const mapFrame = document.getElementById('map')
+    mapFrame.src = generateMapURL(event.target.value)
+  }
+})
 
-NOTE: I have not bothered to extract this behaviour into a separate function
-since this merely serves as an example/guide on how to convert a text address
-into a clickable Google Maps Link. I assume it will be used when displaying the
-meeting info. Note that I have already imposed limits on the characters that
-can be submitted in the form by using in the text input html tag for the
-address (see below), and so this need not be checked here.
-
-required pattern="[A-z0-9À-ž'.,\s+-º]+"
-
-*/
 meetingForm.addEventListener('submit', (event) => {
   event.preventDefault()
-
-  // Build a valid URL
-  const address = event.target.elements.addressInput.value
-  const URL = `${URL_BASE}dir/?api=${API_NUM}&destination=${address}`
-  const encodedURL = encodeURI(URL)
-
-  // Create an anchor element with the URL
-  const a = document.createElement('a')
-  const text = document.createTextNode(`${address}`)
-  a.appendChild(text)
-  a.href = encodedURL
-  a.target = '_blank'
-
-  // Add it to the list
-  const li = document.createElement('li')
-  li.classList.add('list-group-item')
-  li.appendChild(a)
-  addressList.appendChild(li)
+  if (meetingChoice.value == 'none-selected') {
+    window.alert('Please select a viable meeting option')
+  } else if (meetingChoice.value == 'online') {
+    console.log('do something - send information to database')
+  } else if (meetingChoice.value == 'face-to-face') {
+    createDirectionLink()
+    console.log('do something - send information to database')
+  }
 })
 
 /* ---------------------------- Helper Functions ---------------------------- */
@@ -82,4 +77,147 @@ function generateMapURL (address) {
 
   // Encode the URL before returning it
   return encodeURI(URL)
+}
+
+/* -- Convert submitted address to Google Map link -- */
+function createDirectionLink () {
+// Build a valid URL
+  const address = document.getElementById('addressInput').value
+  // const address = event.target.elements.addressInput.value
+  const URL = `${URL_BASE}dir/?api=${API_NUM}&destination=${address}`
+  const encodedURL = encodeURI(URL)
+
+  // Create an anchor element with the URL
+  const a = document.createElement('a')
+  const text = document.createTextNode(`${address}`)
+  a.appendChild(text)
+  a.href = encodedURL
+  a.target = '_blank'
+
+  // Add it to the list
+  const li = document.createElement('li')
+  li.classList.add('list-group-item')
+  li.appendChild(a)
+  addressList.appendChild(li)
+}
+
+/* ---- Further helper functions used to update the contents of the HTML ---- */
+
+function removePlace (placeDiv) {
+  while (placeDiv.hasChildNodes()) {
+    placeDiv.removeChild(placeDiv.lastChild)
+  }
+}
+
+function loadPlatform () {
+  const placeDiv = document.getElementById('place')
+  removePlace(placeDiv)
+
+  // create the platform division
+  const platform_div = document.createElement('div')
+
+  // create label
+  const label = document.createElement('label')
+  label.setAttribute('for', 'platformInput')
+  label.setAttribute('class', 'form-label')
+  label.innerHTML = 'Choose Platform'
+
+  // create the platform explanation
+  const platform_exp = document.createElement('div')
+  platform_exp.setAttribute('class', 'form-text')
+  platform_exp.innerHTML = 'Please enter the name of the platform that will be used for the meeting'
+
+  // create the input for the platform
+  const inputPlatform = document.createElement('input')
+  inputPlatform.type = 'text'
+  inputPlatform.setAttribute('class', 'form-control')
+  inputPlatform.setAttribute('id', 'platformInput')
+  inputPlatform.setAttribute('required', 'required')
+  inputPlatform.setAttribute('pattern', "[A-z0-9À-ž'.,\\s+-]+")
+  inputPlatform.setAttribute('title', 'Platform name must consist of alphanumeric characters')
+  inputPlatform.setAttribute('placeholder', 'Enter platform name...')
+  inputPlatform.setAttribute('aria-describedby', 'platformHelp')
+
+  // append
+  platform_div.appendChild(label)
+  platform_div.appendChild(platform_exp)
+  platform_div.appendChild(inputPlatform)
+  placeDiv.appendChild(platform_div)
+
+  // Create Link division for input
+  // create the link division
+  const linkDiv = document.createElement('div')
+
+  // create the link explanation
+  const linkExp = document.createElement('div')
+  linkExp.setAttribute('class', 'form-text')
+  linkExp.setAttribute('id', 'linkHelp')
+  linkExp.innerHTML = 'Please enter the name of the platform that will be used for the meeting'
+
+  // create label
+  const linkLabel = document.createElement('label')
+  linkLabel.setAttribute('for', 'linkInput')
+  linkLabel.setAttribute('class', 'form-label')
+  linkLabel.innerHTML = 'Please enter the link for the meeting'
+
+  // create the input for the link
+  const inputLink = document.createElement('input')
+  inputLink.type = 'text'
+  inputLink.setAttribute('class', 'form-control')
+  inputLink.setAttribute('id', 'platformInput')
+  inputLink.setAttribute('required', 'required')
+  inputLink.setAttribute('pattern', "[A-z0-9À-ž'.,\\s+-@:%_~#=&?//\]+")
+  inputLink.setAttribute('title', 'link name must consist of alphanumeric characters')
+  inputLink.setAttribute('placeholder', 'Enter the link to the meeting...')
+  inputLink.setAttribute('aria-describedby', 'linkHelp')
+
+  linkDiv.appendChild(linkLabel)
+  linkDiv.appendChild(linkExp)
+  linkDiv.appendChild(inputLink)
+  linkDiv.appendChild(divTRY)
+  placeDiv.appendChild(linkDiv)
+}
+
+function loadLocation () {
+  const placeDiv = document.getElementById('place')
+  removePlace(placeDiv)
+
+  // create label
+  const label = document.createElement('label')
+  label.setAttribute('for', 'addressInput')
+  label.setAttribute('class', 'form-label')
+  label.innerHTML = 'Choose location'
+
+  // create the input
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.setAttribute('class', 'form-control')
+  input.setAttribute('id', 'addressInput')
+  input.setAttribute('required', 'required')
+  input.setAttribute('pattern', "[A-z0-9À-ž'.,\\s+-º]+")
+  input.setAttribute('title', 'Location must consist of alphanumeric characters or use coordinate notation')
+  input.setAttribute('placeholder', 'Enter address...')
+  input.setAttribute('aria-describedby', 'addressHelp')
+
+  // create the map explanation
+  const map_div = document.createElement('div')
+  map_div.setAttribute('class', 'form-text')
+  map_div.setAttribute('id', 'addressHelp')
+  map_div.innerHTML = 'View the map below to confirm that the correct address has been found'
+
+  // create the map frame
+  const map_frame = document.createElement('iframe')
+  map_frame.setAttribute('id', 'map')
+  map_frame.setAttribute('class', 'rounded')
+  map_frame.setAttribute('width', '100%')
+  map_frame.setAttribute('height', '400')
+  map_frame.setAttribute('frameborder', '0')
+  map_frame.setAttribute('style', 'border:0')
+  map_frame.setAttribute('src', 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCx_ZKS9QvVboI8DL_D9jDGA4sBHiAR3fU&q=%20&zoom=13')
+  map_frame.setAttribute('allowfullscreen', 'allowfullscreen')
+
+  placeDiv.appendChild(label)
+  placeDiv.appendChild(input)
+  placeDiv.appendChild(map_div)
+  placeDiv.appendChild(map_frame)
 }
