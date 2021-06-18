024212e22d11f6e1b80d5cb8331584ccbcf518ff
TaliWeinstein <tali.jae@gmail.com>
2021-06-18 10:52:05 +0200

    refactor (covid-screening ): Commenting the code and removing non functional code

--- public/scripts/covid-screening.js
+++ public/scripts/covid-screening.js
@@ -5,28 +5,25 @@ import { UserService } from './UserService.js'
 
 const userService = UserService.getUserServiceInstance()
 let currentUser = null
+
+// Submits the Covid Screening results captured by the form
 const submissionButton = document.getElementById('submission')
 submissionButton.addEventListener('click', (event) => {
   event.preventDefault()
-  processCovidScreening()
+  updateScreening()
 })
 
-
-
-function processCovidScreening () {
-// Possibly replace with better pop up
-  alert('Information Captured')
-  updateScreening()
-}
-
+// Determines whether the person has passed the screening or not based on logic
+// in Wits Covid screenign form
 function getAllSelectedAnswers () {
   const symptomsQ1 = document.getElementById('SymptomsQ1').value
   const symptomsQ2 = document.getElementById('SymptomsQ2').value
   const contactQ1 = document.getElementById('ContactQ1').value
   const contactQ2 = document.getElementById('ContactQ2').value
+  // Currently unused - for expanded functionality could send a pop up to say that you are high risk if
+  // the perosn answers yes
   const contactQ3 = document.getElementById('ContactQ3').value
   const contactQ4 = document.getElementById('ContactQ4').value
-
 
   if (symptomsQ1 === 'yes' || symptomsQ2 === 'yes' || contactQ1 === 'yes' || contactQ2 === 'yes') {
     return 0
@@ -35,6 +32,7 @@ function getAllSelectedAnswers () {
   }
 }
 
+// Updates screening result in correct form for posting to database
 function updateScreening () {
   userService.getCurrentUser().then(
     user => {
@@ -49,6 +47,7 @@ function updateScreening () {
     })
 }
 
+// Posts results to the database
 function addScreeningResult (newScreening) {
   fetch('/create-screening', {
     method: 'POST',
