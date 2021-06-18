9d7d8c6760f192de9a8665ed9c8e0195121a2432
tarryn-maggs <tarrynmaggs@gmail.com>
2021-06-18 11:33:05 +0200

    feat: added profile view to the main dashboard

--- public/scripts/memberships.js
+++ public/scripts/memberships.js
@@ -1,28 +1,6 @@
-import { UserService } from './UserService.js'
-const userService = UserService.getUserServiceInstance()
-let currentUser = null
+import { RetrieveProfile, LeaveGroup } from './load-profile.js'
 document.addEventListener('DOMContentLoaded', function () {
-  userService.getCurrentUser().then(
-    user => {
-      currentUser = user
-      const welcomeDiv = document.getElementById('welcome-div')
-      const welcomeHeading = document.createElement('h2')
-      welcomeHeading.textContent = `Welcome, ${currentUser.username} with ID ${currentUser.id}`
-      welcomeDiv.appendChild(welcomeHeading)
-
-      fetch('http://localhost:3000/profileViews/' + currentUser.id)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          loadProfile(data)
-          fetch('http://localhost:3000/membershipViews/' + currentUser.id)
-            .then(response => response.json())
-            .then(data => {
-              console.log(data)
-              loadHTMLTable(data)
-            })
-        })
-    })
+  RetrieveProfile()
 })
 
 document.querySelector('table tbody').addEventListener('click', function (event) {
@@ -30,58 +8,3 @@ document.querySelector('table tbody').addEventListener('click', function (event)
     LeaveGroup(event.target.dataset.id)
   }
 })
-
-function loadHTMLTable (data) {
-  const table = document.querySelector('table tbody')
-  console.log(data.recordset.length)
-  if (data.recordsets.length === 0) {
-    table.innerHTML = "<tr><td class='no-data' colspan='6'>No Memberships</td></tr>"
-    return
-  }
-
-  let tableHtml = ''
-  userService.getCurrentUser().then(
-    user => {
-      currentUser = user
-      data.recordset.forEach(function ({ group_id, group_name, date_joined, membership_id }) { // group_num, group_online, group_url }) {
-        tableHtml += '<tr>'
-        tableHtml += `<td id='${group_id}-id'>${group_id}</td>`
-        tableHtml += `<td id = '${group_id}-group-name'><a href='/chat?username=${currentUser.username}&group=${group_name}'>${group_name}</a></td>`
-        tableHtml += `<td id = '${group_id}-date-joined'>${date_joined}</td>`
-        tableHtml += `<td id = '${group_id}-num-memb'>group_num</td>`// ${group_num}</td>`
-        tableHtml += `<td id = '${group_id}-num-online'>group_online</td>`// ${group_online}</td>`
-        tableHtml += `<td><button class="delete-row-btn" data-id=${membership_id}>Leave</td>`
-        tableHtml += '</tr>'
-      })
-      table.innerHTML = tableHtml
-    })
-}
-
-function loadProfile (data) {
-  const profile = document.querySelector('#user_details')
-  if (data.recordset.length === 0) {
-    profile.innerHTML = "<p class='no-data'>No Informations</p>"
-    return
-  }
-  let tableHtml = ''
-
-  data.recordset.forEach(function ({ username, first_name, last_name, rating }) { // group_num, group_online, group_url }) {
-    tableHtml += `<p id='${username}-id'>Username: ${username}</p>`
-    tableHtml += `<p id='${first_name}-id'>First name: ${first_name}</p>`
-    tableHtml += `<p id='${last_name}-id'>Last name: ${last_name}</p>`
-    tableHtml += `<p id='${rating}-id'>rating: ${rating}</p>`
-  })
-  profile.innerHTML = tableHtml
-}
-
-function LeaveGroup (membership_id) {
-  fetch('http://localhost:3000/delete/' + membership_id, {
-    method: 'DELETE'
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        location.reload()
-      }
-    })
-}
