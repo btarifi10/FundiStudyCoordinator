927b5cdb715e3d41b5e0864f70f76fd5a8cfa818
TaliWeinstein <tali.jae@gmail.com>
2021-06-24 19:04:30 +0200

    feat(covid-screenings): Enabling ability to route to covid screening page directly from chat screen

--- public/scripts/covid-screening.js
+++ public/scripts/covid-screening.js
@@ -11,21 +11,40 @@ const submissionButton = document.getElementById('submission')
 submissionButton.addEventListener('click', (event) => {
   event.preventDefault()
   updateScreening()
+  alert('Screening results captured')
 })
+
+// Retrieves the new entry for each form element
+// function getNewEntry (value) {
+//   for (let i = 0; i < value.length; i++) {
+//     if (value[i].checked) {
+//       return parseInt(value[i].value, 10)
+//     }
+//   }
+// }
 
 // Determines whether the person has passed the screening or not based on logic
 // in Wits Covid screenign form
-
 function getAllSelectedAnswers () {
-  const symptomsQ1 = document.getElementById('SymptomsQ1').value
-  const symptomsQ2 = document.getElementById('SymptomsQ2').value
-  const contactQ1 = document.getElementById('ContactQ1').value
-  const contactQ2 = document.getElementById('ContactQ2').value
+  // const symptomsQ1 = getNewEntry(document.getElementsByName('SymptomsQ1'))
+  // const symptomsQ2 = getNewEntry(document.getElementsByName('SymptomsQ2'))
+  // const contactQ1 = getNewEntry(document.getElementsByName('ContactQ1'))
+  // const contactQ2 = getNewEntry(document.getElementsByName('ContactQ2'))
+
+  // // Currently unused - for expanded functionality could send a pop up to say that you are high risk if
+  // // the perosn answers yes
+  // const contactQ3 = getNewEntry(document.getElementsByName('ContactQ3'))
+  // const contactQ4 = getNewEntry(document.getElementsByName('ContactQ4'))
+
+  const symptomsQ1 = document.getElementsByName('SymptomsQ1').value
+  const symptomsQ2 = document.getElementsByName('SymptomsQ2').value
+  const contactQ1 = document.getElementsByName('ContactQ1').value
+  const contactQ2 = document.getElementsByName('ContactQ2').value
 
   // Currently unused - for expanded functionality could send a pop up to say that you are high risk if
   // the perosn answers yes
-  const contactQ3 = document.getElementById('ContactQ3').value
-  const contactQ4 = document.getElementById('ContactQ4').value
+  const contactQ3 = document.getElementsByName('ContactQ3').value
+  const contactQ4 = document.getElementsByName('ContactQ4').value
 
   if (symptomsQ1 === 'yes' || symptomsQ2 === 'yes' || contactQ1 === 'yes' || contactQ2 === 'yes') {
     return 0
@@ -60,5 +79,4 @@ function addScreeningResult (newScreening) {
     },
     body: JSON.stringify(newScreening)
   })
-  window.history.back()
 }
