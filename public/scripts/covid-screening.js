d8247f75a4ce60d5a3334e23c91a4a638ea9b6a8
TaliWeinstein <tali.jae@gmail.com>
2021-06-24 19:09:38 +0200

    feat(covid-screening): Enable the user to be routed back to the chat once the covid-screening has completed

--- public/scripts/covid-screening.js
+++ public/scripts/covid-screening.js
@@ -12,30 +12,12 @@ submissionButton.addEventListener('click', (event) => {
   event.preventDefault()
   updateScreening()
   alert('Screening results captured')
+  location.href = '/chat';
 })
-
-// Retrieves the new entry for each form element
-// function getNewEntry (value) {
-//   for (let i = 0; i < value.length; i++) {
-//     if (value[i].checked) {
-//       return parseInt(value[i].value, 10)
-//     }
-//   }
-// }
 
 // Determines whether the person has passed the screening or not based on logic
 // in Wits Covid screenign form
 function getAllSelectedAnswers () {
-  // const symptomsQ1 = getNewEntry(document.getElementsByName('SymptomsQ1'))
-  // const symptomsQ2 = getNewEntry(document.getElementsByName('SymptomsQ2'))
-  // const contactQ1 = getNewEntry(document.getElementsByName('ContactQ1'))
-  // const contactQ2 = getNewEntry(document.getElementsByName('ContactQ2'))
-
-  // // Currently unused - for expanded functionality could send a pop up to say that you are high risk if
-  // // the perosn answers yes
-  // const contactQ3 = getNewEntry(document.getElementsByName('ContactQ3'))
-  // const contactQ4 = getNewEntry(document.getElementsByName('ContactQ4'))
-
   const symptomsQ1 = document.getElementsByName('SymptomsQ1').value
   const symptomsQ2 = document.getElementsByName('SymptomsQ2').value
   const contactQ1 = document.getElementsByName('ContactQ1').value
@@ -70,7 +52,6 @@ function updateScreening () {
 }
 
 // Posts results to the database
-
 function addScreeningResult (newScreening) {
   fetch('/create-screening', {
     method: 'POST',
